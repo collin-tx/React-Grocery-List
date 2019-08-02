73599f96fb4a5873df82949e9cc0a13425c09c95
Collin Smith <cmsmith.tx@gmail.com>
2019-08-02 14:51:20 -0500

    Added edit functionality to list item component and styles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ export default class App extends Component {
 	state = {
 		websiteName: 'My Grocery List',
 		groceries: [],
-		value: ''
+		value: '',
+		grandTotal: ""
 	};
 
 	componentDidMount(){
@@ -19,6 +20,7 @@ export default class App extends Component {
 
 	handleSubmit = (e) => {
 		e.preventDefault();
+		if (this.state.value.length > 0){
 		this.setState({
 			groceries: [...this.state.groceries, 
 				{
@@ -29,6 +31,9 @@ export default class App extends Component {
 			],
 			value: ''
 		})
+	} else {
+		alert("You can't add nothing to your grocery list!")
+	}
 	}
 
 	handleChange = (e) => {
@@ -44,20 +49,44 @@ export default class App extends Component {
 		})
 	}
 
-	render() {
-		let allGroceries = this.state.groceries.map((item) => {
-			return <ListItem item={item} id={item.id} removeItem={this.removeItem} />
+	editItem = (index, newItem) => {
+		const newList = this.state.groceries;
+		newList[index].name = newItem;
+		this.setState({
+			groceries: newList
 		})
+	}
+
+	getTotal = () => {
+		let prices = [];
+		for (let item in this.state.groceries){
+			prices.push(this.state.groceries[item].price);
+		}
+		let total = prices.reduce((a, b) => a + b);
+		this.setState({
+			grandTotal: total
+		})
+
+		let totalP = document.getElementById('total');
+		totalP.className = "bordered";
+	}
+
+	render() {
+		let allGroceries = this.state.groceries.map((item, index) => {
+			return <ListItem item={item} id={item.id} index={index} removeItem={this.removeItem} editItem={this.editItem} />
+		})
+
 		return (
 			<main>
 				<div className="container-fluid">
 					<div className="row">
-						<h1>{this.state.websiteName}</h1>
+						<h1 className="title">{this.state.websiteName}</h1>
 					</div>
 					<div className="row">
 						<form onSubmit={this.handleSubmit}>
-							<input type="text" className="form-control" value={this.state.value} onChange={this.handleChange} />
-							<button className="btn btn-primary btn-block">+</button>
+							<input type="text" className="form-control top" placeholder="Add an item..." 
+							value={this.state.value} onChange={this.handleChange} id="userInput" />
+							<button className="btn btn-primary btn-block top">+</button>
 						</form>
 					</div>
 					<div>
@@ -65,39 +94,10 @@ export default class App extends Component {
 							{allGroceries}
 						</ul>
 					</div>
+					<button className="btn btn-secondary float-right" onClick={this.getTotal}>Get My Total</button>
+					<p id="total" className="hide">Grand total: ${this.state.grandTotal}</p>
 				</div>
 			</main>
 		);
 	}
 }
-
-// CHOOSE ONE OF THE PROMPTS BELOW FOR YOUR PROJECT 1
-
-// Grocery List Project
-// BRIEF: Create a grocery list application that will render out a grocery list of items from the JSON data
-// Each item should display a name and a price at minimum.
-// 1. There should be an input field and a button that allows a user to add a grocery item to the list
-// 2. Your grocery list should be kept in a json file and then loaded into the State of your application.
-// 3. Each grocery list item should be a new Component and you will pass the items information to the list with props
-// 4. You should use the .map function to iterate the items in the list into Components
-// 5. After the user adds the grocery item, make sure to clear the input for a good user experience!
-// 6. You should be able to remove items from the Grocery list
-
-// BONUSES:
-// 1. Can you make it so that each item in the list could be editable?
-// 2. Can you make a button or a value that would print out the total of each item in the list?
-// 3. Can you do data validation so that if someone trys to submit an input without any text in it they get an error?
-
-// ## Chat Bot Project
-// * *BRIEF*: Create a chat bot application that will allow a user to communicate with a Bot.
-// * 1. There should be an input field and a button that allows a user to add a message item to the conversation
-// * 2. The chatbot should utilize JSON data to store canned responses from the bot!
-// * 3. The conversation should be stored in the State object of a Component, rendered initially from a JSON file.
-// * 4. You should use the .map function to iterate the messages into into Components
-// * 5. After the user adds a message item, make sure to clear the input for a good user experience!
-
-// * BONUSES:
-// * 1. Can you make it so a user could delete a message?
-// * 2. Can you make it so that there is a delay between when the user submits their message and the chatbot responds?
-// * 3. Can you do data validation to make sure that the user has typed a message before allowing them to submit?
-// * 4. Can you make the robot respond dynamically to a message if they robot does not understand it. (for example, if the robot asks "Do you want to eat icecream or cookies" and the user says "a burger" can you make the robot say "Sorry, I do not understand?")
--- src/Components/ListItems.js
+++ src/Components/ListItems.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 
 export default class ListItem extends Component {
 
+state = {
+	editing: false,
+	currentValue: ''
+}
 
+edit = () => {
+	if (this.state.editing){
+		this.props.editItem(this.props.index, this.state.currentValue)
+		this.setState({
+			editing: false
+		});
+	} else {
+		this.setState({
+			editing: true
+		});
+	}
+}
+
+handleChange = (e) => {
+	this.setState({
+		currentValue: e.target.value
+	})
+}
 	render() {
 		return (
 			<li className="list-group-item text-left">
 				<b>{this.props.item.name} | ${this.props.item.price}</b>
-				<button className="btn btn-danger" id="remove" onClick={() => this.props.removeItem(this.props.id)}>Remove</button>
+				{this.state.editing ? (
+					<input type="text" onChange={this.handleChange} value={this.currentValue} />
+				) : ('')
+				}
+				<button className="btn btn-success m-1" id="edit" onClick={this.edit}>{this.state.editing ? 'Save' : 'Edit'}</button>
+				<button className="btn btn-danger" id="remove" onClick={() => this.props.removeItem(this.props.id)}>
+					Remove
+					</button>
 			</li>
 		);
 	}
